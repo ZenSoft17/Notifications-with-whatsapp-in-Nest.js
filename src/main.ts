d13e05f6820c5bbfc8136bd375855fd9654c5344
ZenSoft17 <jhojanestebanlopezrios1234@gmail.com>
2025-03-09 13:01:10 -0500

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { ValidationPipe } from '@nestjs/common'
-import { ExpressAdapter } from '@nestjs/platform-express'
-import * as express from 'express'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
-const server = express()
+const server = express();
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, new ExpressAdapter(server))
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-	app.useGlobalPipes(
-		new ValidationPipe({
-			transform: true,
-		})
-	)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
+  );
 
-	await app.init()
+  await app.init();
 }
 
-bootstrap()
+bootstrap().then(() => console.log('NestJS app initialized'));
 
-export default server
+export default server;
